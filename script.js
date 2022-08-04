fbdcd58c089b3c58f10381cd99897ca35df4728f
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-08-03 23:26:20 -0300

    atualizações projeto

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const pixelBoard = document.querySelector('#pixel-board');
+let pixelBoard = document.querySelector('#pixel-board');
 let framePalete = document.querySelectorAll('color-palette');
 let pixel = document.getElementsByClassName('pixel');
-let clear = document.querySelector('#clear-board');
 let colors = document.getElementsByClassName('color');
 let btnLimpar = document.querySelector('#clear-board');
+let size1 = document.querySelector('#board-size')
+let size = size1.value;
+let color = document.querySelector(".color")
 
 function frame(size) {
   pixelBoard.style.setProperty('--size', size); // reference: https://www.youtube.com/watch?v=wZZyhrJxZRU
   for (let index = 0; index < size * size; index += 1) {
-    const div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('pixel'); // criando uma classe nova
+
+    div.addEventListener('mouseover', function(){
+      div.style.backgroundColor = color.value
+
+    });
     pixelBoard.appendChild(div); // ligando o pai ao filho
   }
 }
 
-frame(5);
+frame(size);
 
 function classSelect(evento) {
   let select = document.querySelector('.selected');
@@ -26,13 +33,12 @@ function addEvento() {
   for (let index = 0; index < colors.length; index += 1) {
     colors[index].addEventListener('click', classSelect);
   }
-} 
+}
 addEvento();
 
 function clearFream() {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
-
   }
-} 
+}
 btnLimpar.addEventListener('click', clearFream);
